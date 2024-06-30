0272e149547d72a0c8620c75817c0b9e01c9e43b
thiateleu <thiateleu@gmail.com>
2024-06-30 10:45:06 -0400

    lab assignment

--- data-collection.js
+++ data-collection.js
@@ -1,12 +1,24 @@
-//////////////Part 1: fizz buzz////////////////
-for (let i = 1; i <= 100; i++) {
-    if ( i % 3 === 0 && i %5 === 0) {
-        console.log("fizz buzz");
-    } else if ( i % 3 === 0) {
-        console.log("fizz");
-    } else if (i % 5 === 0) {
-        console.log("buzz");
-    } else {
-        console.log(i);
+//--------Part 2: Expanding Functionality-------
+
+let csvData = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctors Assistant,26'
+
+//step 1
+let rows = csvData.split('\n').map(row => row.split(','));
+
+//step 2
+let columnCount = rows[0].length;
+
+console.log("Number of columns:" , columnCount)
+
+//step 3
+let resultArray = rows.map(row => {
+    if (row.length !== columnCount) {
+        throw new Error('Row length does not match')
     }
-}
+    return row;
+});
+
+//step 4
+let cachedArray = resultArray;
+
+console.log('Two-Dimensional Array:', cachedArray);
